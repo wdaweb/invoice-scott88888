df4cb06a5da3d359e0be94fc920b6a6f4d17277a
scott <pig19850813@gmail.com>
2020-05-08 13:51:02 +0800

    first

---
+++ ConnectTitle.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>發票喔</title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>   
+</head>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include "ConnectTitle.php"?>
+<head>
+<?php include "Style.css"?>
+</head>
+<body>
+<?php include_once "ConnectPdo.php"?>
+
+
+</body>
+</html>
